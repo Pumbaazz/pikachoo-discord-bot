2b93f8658ea349ab22b872e9b7563cc3553ba4d6
PumbaazZ <nguyntunphng@gmail.com>
2021-02-01 17:28:45 +0700

    hi and avatar

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");	
+const config = require("./config.json");
+const PREFIX = '!';
+
+var bot = new Discord.Client();
+
+
+bot.on('ready', function()
+{	  
+    console.log("bot is now online");	
+})	
+bot.on("message", async message => {	
+    var args = message.content.substring(PREFIX.length).split(" ");
+    
+    if (!message.content.startsWith(PREFIX)) return;	   
+    switch (args[0].toLowerCase()) {	        
+        case "ping":	            
+            message.channel.send("pong!");	            
+            break;	        
+        case "hi":	            
+            message.channel.send("hi");	            
+            break;
+        case "avatar":
+            message.reply(message.author.displayAvatarURL({size:4096 }));
+            break;
+    }
+    	
+});
+
+bot.login(config.token);
